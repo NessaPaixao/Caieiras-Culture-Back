90716b5d225193c7b472ff53da3e058467cecf17
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-10-30 17:34:29 -0300

    Adicionar model da pessoa maior de idade

--- maior_dezoito/models.py
+++ maior_dezoito/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Responsavel(models.Model):
+    nome_completo = models.CharField( max_length=50,verbose_name='Nome Completo')
+    idade = models.IntegerField(max_length=50, verbose_name='Idade')
+    genero = models.CharField(max_length=50, verbose_name='Gênero')
+    cpf = models.IntegerField(max_length=50, verbose_name='CPF')
+    contato = models.IntegerField(max_length=50, verbose_name='Contato')
+    cep = models.IntegerField(max_length=50, verbose_name='CEP')
+    email = models.CharField(max_length=50, verbose_name='Email')
+    senha = models.CharField(max_length=50, verbose_name='Senha')
