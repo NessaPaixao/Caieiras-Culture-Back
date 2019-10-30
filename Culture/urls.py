13823df32a5f0d7891337211a7b53968ac63ede7
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-10-30 17:58:43 -0300

    Adicionar rotas de responsavel +18

--- Culture/urls.py
+++ Culture/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from maior_dezoito.views import ResponsavelViewSet
+
+router = routers.DefaultRouter()
+router.register(r'responsavel', ResponsavelViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
+
