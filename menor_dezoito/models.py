db9e75aeb756c20b5b9f78e6215f37667de57ef5
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-10-31 16:03:17 -0300

    Ajustar erros

--- menor_dezoito/models.py
+++ menor_dezoito/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Menor(models.Model):
-    nome_responsavel = models.CharField( max_length=50,verbose_name='Nome completo do Responsavel')
+    nome_responsavel = models.CharField( max_length=50,verbose_name='Nome do Responsavel')
     idade_resp = models.IntegerField(max_length=50, verbose_name='Idade do responsavel')
-    cpf = models.IntegerField(max_length=50, verbose_name='CPF')
     genero_resp = models.CharField(max_length=50, verbose_name='Gênero do responsavel')
+    cpf = models.IntegerField(max_length=50, verbose_name='CPF')
     contato = models.IntegerField(max_length=50, verbose_name='Contato')
     cep = models.IntegerField(max_length=50, verbose_name='CEP')
-    nome_menor = models.CharField(max_length=50, verbose_name='Nome completo do menor')
+    nome_menor = models.CharField(max_length=50, verbose_name='Nome do menor')
     idade_menor = models.IntegerField(max_length=50, verbose_name='Idade do menor')
     genero = models.CharField(max_length=50, verbose_name='Gênero do menor')
     email = models.CharField(max_length=50, verbose_name='Email')
