fd3a047b17d42c857c1ef5c47fd4970e5a6cb273
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-10-31 15:51:01 -0300

    Adicionar model do menor

---
+++ menor_dezoito/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MenorDezoitoConfig(AppConfig):
+    name = 'menor_dezoito'
---
+++ menor_dezoito/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Menor(models.Model):
+    nome_responsavel = models.CharField( max_length=50,verbose_name='Nome completo do Responsavel')
+    idade_resp = models.IntegerField(max_length=50, verbose_name='Idade do responsavel')
+    cpf = models.IntegerField(max_length=50, verbose_name='CPF')
+    genero_resp = models.CharField(max_length=50, verbose_name='Gênero do responsavel')
+    contato = models.IntegerField(max_length=50, verbose_name='Contato')
+    cep = models.IntegerField(max_length=50, verbose_name='CEP')
+    nome_menor = models.CharField(max_length=50, verbose_name='Nome completo do menor')
+    idade_menor = models.IntegerField(max_length=50, verbose_name='Idade do menor')
+    genero = models.CharField(max_length=50, verbose_name='Gênero do menor')
+    email = models.CharField(max_length=50, verbose_name='Email')
+    senha = models.CharField(max_length=50, verbose_name='Senha')
