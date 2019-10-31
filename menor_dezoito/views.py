4792f7f3bcde9409b96920977539c762bc92a2b7
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-10-31 15:59:33 -0300

    Adicionar views do menor

--- menor_dezoito/views.py
+++ menor_dezoito/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from menor_dezoito.models import Menor
+from menor_dezoito.serializers import MenorSerializer
 
-# Create your views here.
+
+class MenorViewSet(viewsets.ModelViewSet):
+    queryset =Menor.objects.all()
+    serializer_class = MenorSerializer
