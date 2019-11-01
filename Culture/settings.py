c88e9cb3c078ba07b159a3ff6256fd3788785c02
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-11-01 17:38:43 -0300

    Resolver bugs

--- Culture/settings.py
+++ Culture/settings.py
@@ -90,7 +90,7 @@ CORS_ORIGIN_WHITELIST = [
     "https://example.com",
     "https://sub.example.com",
     "http://localhost:3000",
-    "http://localhost:8080",
+    "http://localhost:8000",
     "http://127.0.0.1:9000"
 ]
 # Password validation
@@ -125,6 +125,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
